8f53cf6fe5daa8eeeba1be6b459a03a252f98c7e
GANESHJAGANNATHTELASANG <ganesh.telasang8@gmail.com>
2026-05-23 17:02:41 +0530

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -53,7 +53,7 @@ router.post("/login", async (req, res) => {
       });
     }
 
-    const token = jwt.sign({ id: user._id }, "ganesh_march_project", {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "5h",
     });
 
