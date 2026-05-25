76098b6f12d96178e1db6cb32cdf6b0d17a2aa1e
GANESHJAGANNATHTELASANG <ganesh.telasang8@gmail.com>
2026-05-25 20:57:32 +0530

    fixed verify token logic

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import authRouter from "./routes/auth.js";
 import noteRouter from "./routes/note.js";
-import dotenv from "dotenv";
-
 import cors from "cors";
 import connectToMongoDB from "./db/db.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-dotenv.config();
 
 app.use("/api/auth", authRouter);
 app.use("/api/note", noteRouter);
 
 app.get("/", (req, res) => {
-  res.send("server is running in port 8787");
+  res.send("server is running");
 });
 
-app.listen(8787, () => {
+app.listen(process.env.PORT || 8787, () => {
   connectToMongoDB();
-  console.log("server is running in port 8787");
+  console.log("server is running");
 });
